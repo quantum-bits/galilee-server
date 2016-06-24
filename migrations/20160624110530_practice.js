5018f6e4983d55e54e6738be684d2fba9e4c7e20
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-06-24 11:09:54 -0400

    Add data model for practice and practice detail

---
+++ migrations/20160624110530_practice.js
@@ -0,0 +1,25 @@
+exports.up = function (knex, Promise) {
+
+    return Promise.all([
+
+        knex.schema.createTableIfNotExists('practice', table => {
+            table.increments('id');
+            table.string('title').notNullable();
+            table.string('description').notNullable();
+        }),
+
+        knex.schema.createTableIfNotExists('practice_detail', table => {
+            table.increments('id');
+            table.string('title').notNullable();
+            table.string('description').notNullable();
+            table.integer('practice_id').references('practice.id');
+        })
+    ])
+};
+
+exports.down = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfExists('practice_detail'),
+        knex.schema.dropTableIfExists('practice')
+    ])
+};
