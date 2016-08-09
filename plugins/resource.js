4c443bc00a4615db064af129d23c6b54ea4f5005
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-08-09 18:13:47 -0400

    Add ability to create collections/resources
    * Update all database tables
    * Use UUIDs
    * Partition files into different subdirectories

--- plugins/resource.js
+++ plugins/resource.js
@@ -7,18 +7,15 @@ const uuid = require('uuid');
 const Boom = require('boom');
 const Joi = require('Joi');
 
+const Resource = require('../models/Resource');
 const ResourceType = require('../models/ResourceType');
 const Collection = require('../models/Collection');
 const Config = require('../models/Config');
 
 exports.register = function (server, options, next) {
 
-    const upload_root_promise = Config
-        .query()
-        .where('key', 'upload-root')
-        .first()
-        .then(result => result.value)
-        .catch(err => Boom.badImplementation("No upload root configured", err));
+    // Non-capturing group
+    const extension_re = /^\.(?:gif|jpe?g|png)$/;
 
     server.route({
         method: 'GET',
@@ -37,61 +34,125 @@ exports.register = function (server, options, next) {
 
     server.route({
         method: 'POST',
+        path: '/collections',
+        handler: function (request, reply) {
+            const unique_id = uuid.v4();
+
+            Collection.query()
+                .insert({
+                    id: unique_id,
+                    title: request.payload.title,
+                    description: request.payload.description
+                })
+                .then(() => {
+                    reply({
+                        status: 'ok',
+                        collectionId: unique_id
+                    });
+                })
+                .catch(err => reply(Boom.badImplementation("Can't insert collection", err)));
+        },
+        config: {
+            description: 'Add a new collection',
+            validate: {
+                payload: {
+                    title: Joi.string().required().description('Title'),
+                    description: Joi.string().required().description('Description')
+                }
+            },
+            response: {
+                schema: {
+                    status: Joi.string().valid(['ok']).required(),
+                    collectionId: Joi.string().guid().required()
+                }
+            }
+        }
+    });
+
+    server.route({
+        method: 'POST',
         path: '/resources',
         handler: function (request, reply) {
             const uploadName = path.basename(request.payload.file.filename);
+
+            const uploadExtension = path.extname(uploadName);
+            if (!extension_re.test(uploadExtension)) {
+                return reply(Boom.badData('Invalid file type'));
+            }
+
             const uploadPath = request.payload.file.path;
 
             Config.query()
-                .where('key', 'upload-root')
-                .first()
+                .where('key', 'upload-root').first()
                 .then(result => {
                     const upload_root = result.value;
-                    const destination = path.join(__dirname, upload_root, uploadName);
                     const unique_id = uuid.v4();
+                    const destination = path.join(__dirname, upload_root,
+                        unique_id.substr(0, 2),
+                        unique_id + uploadExtension);
+                    const dest_dir = path.dirname(destination);
 
-                    server.log('info', `upload name ${uploadName}`);
-                    server.log('info', `upload path ${uploadPath}`);
-                    server.log('info', `destination ${destination}`);
-                    server.log('info', `uuid        ${unique_id}`);
+                    fs.mkdir(dest_dir, 0o750, err => {
+                        fs.rename(uploadPath, destination, err => {
+                            if (err) {
+                                return reply(Boom.badImplementation("Can't rename uploaded file", err));
+                            }
 
-                    fs.rename(uploadPath, destination, err => {
-                        if (err) {
-                            reply(Boom.badImplementation("Can't rename uploaded file", err));
-                        }
-
-                        reply({
-                            status: 'ok',
-                            resourceId: unique_id
-                        });
-                    });
-                });
-
+                            Resource.query().insert({
+                                id: unique_id,
+                                caption: request.payload.caption,
+                                copyright_year: request.payload.year,
+                                copyright_owner: request.payload.owner,
+                                details: {
+                                    filename: uploadName
+                                },
+                                resource_type_id: request.payload.typeId
+                            }).then(resource => {
+                                return resource
+                                    .$relatedQuery('collections')
+                                    .relate(request.payload.collectionId)
+                            }).then(() => {
+                                return reply({
+                                    status: 'ok',
+                                    resourceId: unique_id
+                                });
+                            }).catch(err => reply(Boom.badImplementation('Problem with upload', err)));
+                        });     // rename
+                    });         // mkdir
+                });             // query
         },
         config: {
             description: 'Add a new resource',
             payload: {
                 parse: true,
                 output: 'file'
-            },
+            }
+            ,
             validate: {
                 payload: {
                     file: Joi.any().required().description('File being uploaded'),
-                    collectionId: Joi.number().required().description('Resource collection ID')
+                    caption: Joi.string().required().description('Caption for file'),
+                    year: Joi.number().positive().min(1900).description('Copyright year'),
+                    owner: Joi.string().description('Copyright owner'),
+                    collectionId: Joi.string().guid().required().description('Resource collection ID'),
+                    typeId: Joi.number().positive().required().description('Resource type ID')
                 }
-            },
+            }
+            ,
             response: {
                 schema: {
                     status: Joi.string().valid(['ok']).required(),
                     resourceId: Joi.string().guid().required()
                 }
-            },
+            }
+            ,
             cors: {
                 origin: ['http://localhost:4200'],
                 credentials: true
             }
         }
-    });
+    })
+    ;
 
     server.route({
         method: 'GET',
