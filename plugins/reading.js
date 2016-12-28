1aa260c1d7996a64788bbea135a0b4bbe2ec6788
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-12-28 15:39:00 -0500

    Make reading date optional
    * If provided, return readings for supplied date.
    * If absent, return readings for today.

--- plugins/reading.js
+++ plugins/reading.js
@@ -8,28 +8,35 @@ exports.register = function (server, options, next) {
 
     server.route({
         method: 'GET',
-        path: '/readings',
+        path: '/readings/{date?}',
         handler: function (request, reply) {
+            let query_date = request.params.date || new Date();
             ReadingDay
                 .query()
+                .where('date', query_date)
                 .first()
                 .eager('[readings.[practices, collections.resources.[type, tags]]]')
                 .then(reading_day => {
-                    let promises = [];
-                    reading_day.readings.map(reading => {
-                        server.log('info', reading);
-                        let p = new Promise((resolve, reject) => {
-                            server.methods.bg_passage('NKJV', reading.osis_ref, (err, result) => {
-                                if (err) {
-                                    return reply(Boom.notFound(`Bible Gateway ${err}`));
-                                }
-                                reading.text = result.data[0].passages[0].content;
-                                return resolve(result);
-                            })
+                    server.log('info', "Reading day", reading_day);
+                    if (reading_day) {
+                        let promises = [];
+                        reading_day.readings.map(reading => {
+                            server.log('info', reading);
+                            let p = new Promise((resolve, reject) => {
+                                server.methods.bg_passage('NKJV', reading.osis_ref, (err, result) => {
+                                    if (err) {
+                                        return reply(Boom.notFound(`Bible Gateway ${err}`));
+                                    }
+                                    reading.text = result.data[0].passages[0].content;
+                                    return resolve(result);
+                                })
+                            });
+                            promises.push(p);
                         });
-                        promises.push(p);
-                    });
-                    Promise.all(promises).then(res => reply(reading_day.asJson()));
+                        Promise.all(promises).then(res => reply(reading_day.asJson()));
+                    } else {
+                        reply(Boom.badRequest(`No readings for ${request.params.date}`));
+                    }
                 })
                 .catch(err => Boom.badImplementation(err));
         }
