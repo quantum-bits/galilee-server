e78e926bee76d6d4b5c43f5b00c0d2566060c581
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-01-10 23:03:43 +0700

    Implement readingday endpoint.
    * Introduce id to reading_day and use as FK in reading
    * Get rid of "to JSON" methods
    * Start using moment.js, which is great
    * Make date required for readingday endpoint.
    * Improve seed files to delete all relevant data and to use current date instead of fixed date.

--- migrations/0010_reading.js
+++ migrations/0010_reading.js
@@ -5,13 +5,14 @@ exports.up = function (knex, Promise) {
     return Promise.all([
 
         knex.schema.createTableIfNotExists('reading_day', table => {
-           table.date('date').primary();
-           table.string('name');
+            table.increments('id');
+            table.date('date');
+            table.string('name');
         }),
 
         knex.schema.createTableIfNotExists('reading', table => {
             table.increments('id');
-            table.date('for').references('reading_day.date');
+            table.integer('reading_day_id').references('reading_day.id');
             table.integer('seq');
             table.string('std_ref');
             table.string('osis_ref');
