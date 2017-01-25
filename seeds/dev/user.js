2adb01e3b63897f9ee3e9d2b8b1cd3aea95b0808
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-01-25 16:25:25 -0500

    Repair seed to delete data properly.

--- seeds/dev/user.js
+++ seeds/dev/user.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const User = require('../../models/User');
-const loremIpsum = require('lorem-ipsum')
+const loremIpsum = require('lorem-ipsum');
 
 
 exports.seed = function (knex, Promise) {
     return Promise.all([
+        knex('journal_entry_tag').del(),
+        knex('user_tag').del()
+
+    ]).then(() => Promise.all([
         knex('membership').del(),
         knex('user_permission').del(),
         knex('journal_entry').del()
 
-    ]).then(() => Promise.all([
+    ])).then(() => Promise.all([
         knex('permission').del(),
         knex('group').del(),
         knex('user').del()
