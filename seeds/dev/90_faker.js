4864cc348e4a1604870286cd28ab3052c380e8a0
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-01-29 11:25:10 -0500

    Add fake journal entries to seed.
    - Add random number for each user.
    - Include some entries showing updates since creation.

--- seeds/dev/90_faker.js
+++ seeds/dev/90_faker.js
@@ -1,24 +1,27 @@
 'use strict';
 
-const debug = require('debug')('seed');
+const Debug = require('debug');
+const debug = Debug('seed:debug');
+const log = Debug('seed:log');
+
 const faker = require('faker');
 const _ = require('lodash');
 
 const User = require('../../models/User');
 const Version = require('../../models/Version');
 
-function seedVersions() {
-    let all_versions = [];
-    const version_data = [
-        {code: 'ESV', title: 'English Standard Version'},
-        {code: 'KJV', title: 'King James Version'},
-        {code: 'NASB', title: 'New American Standard Version'},
-        {code: 'NIV', title: 'New International Version'},
-        {code: 'NKJV', title: 'New King James Version'},
-        {code: 'RSV', title: 'Revised Standard Version'}
-    ];
+const versionData = [
+    {code: 'ESV', title: 'English Standard Version'},
+    {code: 'KJV', title: 'King James Version'},
+    {code: 'NASB', title: 'New American Standard Version'},
+    {code: 'NIV', title: 'New International Version'},
+    {code: 'NKJV', title: 'New King James Version'},
+    {code: 'RSV', title: 'Revised Standard Version'}
+];
+let versionObjects = [];
 
-    return Promise.all(version_data.map(datum =>
+function seedVersions() {
+    return Promise.all(versionData.map(datum =>
         Version.query().where('code', datum.code).first().then(result => {
             debug("Result %o", result);
             if (result) {
@@ -43,7 +46,7 @@ function seedVersions() {
             // Even if the insert failed, we should be okay, because the
             // catch clause will pick it up.
             debug("Adding %o", version)
-            all_versions.push(version);
+            versionObjects.push(version);
         }).catch(err => {
             console.error("ERR", err);
         })
@@ -51,20 +54,62 @@ function seedVersions() {
 }
 
 function seedUsers() {
-    return Promise.all(_.times(10, n => {
-        let fakeUser = {
+    return Promise.all(_.times(10, n =>
+        User.query().insert({
             email: faker.internet.email(),
             password: faker.internet.password(),
             firstName: faker.name.firstName(),
-            lastName: faker.name.lastName()
-        };
-        debug("User %o", fakeUser);
-        return User.query().insert(fakeUser);
-    }));
+            lastName: faker.name.lastName(),
+            joinedOn: faker.date.past(2),
+            preferredVersionId: faker.random.arrayElement(versionObjects).id
+        })
+    ))
+}
+
+/**
+ * Randomly return true a times out of n.
+ * @param a
+ * @param n
+ * @returns {boolean}
+ */
+function chanceIn(a, n) {
+    let val = faker.random.number({min: 1, max: n});
+    return (val <= a);
+}
+
+function seedJournalEntries() {
+    let today = new Date();
+
+    // The map method will wait for all the promises returned by the insert operation.
+    return User.query().map(user => {
+
+        // Generate a random number of journal entries for this user.
+        let count = faker.random.number({min: 1, max: 10});
+        log("Create %d journal entries for user %d", count, user.id);
+
+        return Promise.all(_.times(count, n => {
+            let create_date = faker.date.past(2);
+            let update_date = create_date;
+            if (chanceIn(2, 5)) {
+                update_date = faker.date.between(create_date, today);
+            }
+            return user.$relatedQuery('journal_entries')
+                .insert({
+                    title: faker.lorem.sentence(),
+                    entry: faker.lorem.paragraphs(),
+                    created_at: create_date,
+                    updated_at: update_date
+                });
+        }));
+    });
 }
 
 exports.seed = function (knex, Promise) {
-    return Promise.join(
-        seedVersions(),
-        seedUsers());
-}
+    return seedVersions().then(() => {
+        return seedUsers();
+    }).then(() => {
+        return seedJournalEntries();
+    }).then(
+        () => log("SUCCESS"),
+        err => console.error("FAIL", err));
+};
