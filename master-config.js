5cf941d03c2f9be20e61e060fea850a096dbbdd3
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-02-16 14:51:44 -0500

    Fish values out of configuration files properly

--- master-config.js
+++ master-config.js
@@ -1,11 +1,29 @@
 // This file provides common access to server configuration information.
 
+const _ = require('lodash');
+const debug = require('debug')('config');
+
 const nconf = require('nconf');
 
+nconf.env(['GALILEE']);
 nconf.file('database', './public.conf.json');
 nconf.file('secrets', './secret.conf.json');
 
-module.exports = nconf;
+// Make sure we have a GALILEE environment variable.
+const envName = nconf.get('GALILEE');
+if (!envName) {
+    throw Error("No GALILEE environment variable");
+}
 
-// console.log("NCONF", nconf);
-// console.log("CONFIG", JSON.stringify(nconf.get(), null, 4));
+// Pull values from the selected environment into the top level
+const selectedEnv = nconf.get(envName);
+if (!selectedEnv) {
+    throw Error(`No Galilee environment '${selectedEnv}'`);
+}
+
+_.forEach(selectedEnv, (v, k) => nconf.set(k, v));
+
+debug("NCONF %O", nconf);
+debug("CONFIG %O", nconf.get());
+
+module.exports = nconf;
